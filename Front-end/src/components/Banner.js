a7f31345bd4d85235f689dba9d7c5b25b9592d1c
Viznem <thinhtruong3344@gmail.com>
2023-11-26 12:55:22 +0700

    Ad reviews section

--- Front-end/src/components/Banner.js
+++ Front-end/src/components/Banner.js
@@ -4,7 +4,7 @@ import Image2 from '../assets/game2.png';
 import Image3 from '../assets/game3.jpg';
 import Image4 from '../assets/game4.png';
 import { TypeAnimation } from 'react-type-animation';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Banner = () => {
@@ -25,32 +25,32 @@ const Banner = () => {
           {/* image */}
           <div className='flex flex-col justify-between relative'>
             <motion.div>
-              <img className='object-cover rounded-xl' src={activeImg} alt=''/>
+              <img className='object-cover rounded-xl' src={activeImg} alt='' />
             </motion.div>
             <div className='flex flex-row absolute bottom-5 right-0 justify-end h-24 pt-5'>
-                <img src={images.img1} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img1)}/>
-                <img src={images.img2} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img2)}/>
-                <img src={images.img3} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img3)}/>
-                <img src={images.img4} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img4)}/>
+              <img src={images.img1} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img1)} />
+              <img src={images.img2} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img2)} />
+              <img src={images.img3} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img3)} />
+              <img src={images.img4} alt="" className='w-24 h-20 rounded-md cursor-pointer' onClick={() => setActiveImage(images.img4)} />
             </div>
           </div>
           {/* text */}
           <div className="flex-1 absolute top-0 px-5 py-5 bg-gray-500/50 w-full ">
             <motion.h1
               variants={fadeIn('down', 0.5)}
-              initial = 'hidden'
+              initial='hidden'
               whileInView={'show'}
-              viewport={{once: false, amount: 0.7 }}
-              className='text-[40px] font-bold leading-[0.8] lg:text-[70px] pb-3' 
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[40px] font-bold leading-[0.8] lg:text-[70px] pb-3'
             >
               <b>Wibu <span>Impact</span></b>
             </motion.h1>
             <motion.div
-             variants={fadeIn('down', 0.3)}
-             initial = 'hidden'
-             whileInView={'show'}
-             viewport={{once: false, amount: 0.7 }}
-             className='text-[15px] lg:text-[22px] font-semibold uppercase leading-[1]'
+              variants={fadeIn('down', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[15px] lg:text-[22px] font-semibold uppercase leading-[1]'
             >
               <span>Best Open World RPG</span>
               <TypeAnimation sequence={[
@@ -59,8 +59,8 @@ const Banner = () => {
                 'Explore the games vast open world alone or team up with friends to take on challenging fights.',
                 2000,
               ]}
-              speed={50}
-              repeat={Infinity}
+                speed={50}
+                repeat={Infinity}
               />
             </motion.div>
           </div>
@@ -69,6 +69,6 @@ const Banner = () => {
     </section>
   );
 };
-  
+
 
 export default Banner;
--- Front-end/src/components/Info.js
+++ Front-end/src/components/Info.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CountUp from 'react-countup';
 import { useInView } from 'react-intersection-observer';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 import { TypeAnimation } from 'react-type-animation';
 
@@ -17,28 +17,28 @@ const Info = () => {
       <div className='container mx-auto'>
         <div className='flex gap-10 items-center'>
           {/*img*/}
-          <motion.div 
-          variants={fadeIn('right',0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{once:false, amount: 0.3 }}
-          className='flex-1 bg-about bg-contain bg-no-repeat h-[500px] mix-blend-lighten bg-top'></motion.div>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[500px] mix-blend-lighten bg-top'></motion.div>
 
           {/*text*/}
           <motion.div
-            variants={fadeIn('left',0.3)}
+            variants={fadeIn('left', 0.3)}
             initial='hidden'
             whileInView={'show'}
-            viewport={{once:false, amount: 0.3 }}
+            viewport={{ once: false, amount: 0.3 }}
             className='flex-1'>
             <h2 className='h2 text-accent'>Game Info</h2>
             <h3 className='h3 mb-4'>Gameplay</h3>
-            <TypeAnimation sequence={[
+            {inView ? <TypeAnimation sequence={[
               'The combat in Wibu Impact is fast-paced and action-packed, with a wide range of characters and weapons to choose from. Each character has unique abilities that can be combined in creative ways, making combat dynamic and engaging. Switching between characters can be done quickly during combat, allowing the player to use ',
               3000,
             ]}
-            speed={80} 
-            />
+              speed={80}
+            /> : null}
             <div className='flex gap-x-6 lg:gap-x-10 mb-12 mt-12 justify-center'>
               <div>
                 <div className='text-[30px] font-tertiary text-gradient mb-2'>
